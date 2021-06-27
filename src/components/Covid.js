34fa9507cee3e8c0efd1ddabec14419ec05485b4
unknown <kjayanti88@yahoo.co.in>
2021-06-27 14:40:16 +0530

    covidtracker commit

--- src/components/Covid.js
+++ src/components/Covid.js
@@ -4,32 +4,37 @@ import axios from "axios";
 
 const Covid = () => {
   const [data, setData] = useState([]);
-  const [istate, setIState] = useState("");
+  const [istate, setIState] = useState("India");
+  const [landingPage, setLandingPage] = useState([]);
+  let active = 0, confirmed = 0, recovered = 0, deaths = 0, lastupdatedtime = 0;
 
+  //Fetching the data from the covid9india api and capturing the statewise data
   const getCovidData = () => {
     axios
       .get("https://api.covid19india.org/data.json")
       .then((res) => {
         const actualData = res.data;
         setData(actualData.statewise);
+        setLandingPage(actualData.statewise[0]);
       })
       .catch((err) => {
         console.log(err);
       });
   };
 
+  active = landingPage.active;
+  confirmed = landingPage.confirmed;
+  recovered = landingPage.recovered;
+  deaths = landingPage.deaths; 
+  lastupdatedtime = landingPage.lastupdatedtime;
+
   useEffect(() => {
     getCovidData();
   }, []);
 
-  let active = 0,
-    confirmed = 0,
-    recovered = 0,
-    deaths = 0,
-    lastupdatedtime = 0;
-
+  //Setting the data based on the selected state
   for (let index of data) {
-    if (index.state == istate) {
+    if (index.state === istate) {   
       active = index.active;
       confirmed = index.confirmed;
       recovered = index.recovered;
@@ -43,29 +48,16 @@ const Covid = () => {
   };
 
   return (
-    <div>
+    <div className="outerContainer">
       <section>
-        <h2>COVID-19 LIVE TRACKER</h2>
-        <div>
-          <select value={istate} onChange={stateHandler}>
-            <option>Choose State</option>
-            {data.map((item, ind) => (
-              <option key={ind} value={item.state}>
-                {item.state}
-              </option>
-            ))}
-          </select>
-        </div>
+        <h1>COVID-19 LIVE TRACKER</h1>
+        
+          <h2 style={{color:"#FF87A0", fontSize:"41px"}}>{istate}</h2>
+        
+        
+        <div className="covidDataDetails">
         <ul>
-          <li className="card">
-            <div className="card_main">
-              <div className="card_inner">
-                <p className="card_name">Location</p>
-                <p className="card_total card_small">{istate}</p>
-              </div>
-            </div>
-          </li>
-          <li className="card">
+          <li className="card act">
             <div className="card_main">
               <div className="card_inner">
                 <p className="card_name">Active</p>
@@ -73,7 +65,7 @@ const Covid = () => {
               </div>
             </div>
           </li>
-          <li className="card">
+          <li className="card conf">
             <div className="card_main">
               <div className="card_inner">
                 <p className="card_name">Confirmed</p>
@@ -81,7 +73,7 @@ const Covid = () => {
               </div>
             </div>
           </li>
-          <li className="card">
+          <li className="card recov">
             <div className="card_main">
               <div className="card_inner">
                 <p className="card_name">Recovered</p>
@@ -89,7 +81,11 @@ const Covid = () => {
               </div>
             </div>
           </li>
-          <li className="card">
+          </ul>
+          </div>
+          <div className="covidDataDetails-2">
+            <ul>
+          <li className="card death">
             <div className="card_main">
               <div className="card_inner">
                 <p className="card_name">Deaths</p>
@@ -97,7 +93,7 @@ const Covid = () => {
               </div>
             </div>
           </li>
-          <li className="card">
+          <li className="card time">
             <div className="card_main">
               <div className="card_inner">
                 <p className="card_name">Last Updated</p>
@@ -106,6 +102,18 @@ const Covid = () => {
             </div>
           </li>
         </ul>
+        </div>
+        {/* //Dropdown to select the state */}
+        <div className="stateDropdown">
+          <h2>Choose State</h2>
+          <select value={istate} onChange={stateHandler}>
+            {data.map((item, ind) => (
+              <option key={ind} value={item.state}>
+                {item.state}
+              </option>
+            ))}
+          </select>     
+        </div>
       </section>
     </div>
   );
